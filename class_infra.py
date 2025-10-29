2d1e95d8b9ff6706410058384deaed109508e459
Manon Tessier <manon.tessier@etu.utc.fr>
2025-10-29 15:14:27 +0100

    class infra 2.0

--- class_infra.py
+++ class_infra.py
@@ -1,10 +1,11 @@
 ##Creating infra class
 class Infra :
-    def __init__(self, infra_id, lengh, infra_type, nb_maisons) :
+    def __init__(self, infra_id, lengh, infra_type, nb_maisons, infra_type_construction) :
         self.infra_id = infra_id
         self.lengh = lengh
         self.infra_type = infra_type
-        self.nb_maisons = nb_maisons 
+        self.nb_maisons = nb_maisons
+        self.infra_type_construction = 0 
         
     def repair_infra(self):
         self.infra_type = "infra_intacte"
@@ -15,4 +16,15 @@ class Infra :
     
     def __radd__ (self): 
         return 0
+    
+    class Type_Infra :
+        def __init__(self, nom, prix, duree):
+            self.nom = nom
+            self.prix = prix
+            self.duree = duree
+
+        def get_type_infra_prix(self):
+            return self.prix
+        
+        
 
