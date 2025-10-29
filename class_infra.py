3f06e736800e4fd9a69961fc63861948774dd8f7
Hamsatou-byte <hamsatoumoussa186@gmail.com>
2025-10-29 11:45:35 +0100

    Update class_infra.py

--- class_infra.py
+++ class_infra.py
@@ -1,3 +1,12 @@
 ##Creating infra class
+class :
+    def __init__(infra_id, lengh, infra_type, longueur:
+        self.infra_id = infra_id
+        self.lengh = lengh
+        self.infra_type = infra_type
+        self.longueur = longueur
+
+    def (self):    # méthode
+        print(f",  {self.nom} et j'ai {self.age} ans.")
 
 
