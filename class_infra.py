ff6e214e26f8a18d5ebf70473c79537a01ea2b8f
Manon Tessier <manon.tessier@etu.utc.fr>
2025-10-29 12:23:11 +0100

    batiment class updated

--- class_infra.py
+++ class_infra.py
@@ -1,16 +1,17 @@
 ##Creating infra class
 class Infra :
-    def __init__(self, infra_id, infra_type, longueur) :
+    def __init__(self, infra_id, lengh, infra_type, nb_maisons) :
         self.infra_id = infra_id
         self.lengh = lengh
         self.infra_type = infra_type
-        self.nb_maisons = nb.maisons 
+        self.nb_maisons = nb_maisons 
         
     def repair_infra(self):
         return 0
     
     def get_infra_difficulty(self):
-        return 0
+        difficulty = self.lengh / self.nb_maisons
+        return difficulty
     
     def __radd__ (self): 
         return 0
